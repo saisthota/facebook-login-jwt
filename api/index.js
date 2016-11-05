e85e99617b32a3162001b8fe0f3be51e92a194dd
saisthota <me@saisthota.com>
2016-11-04 22:23:43 -0700

    Updated Bears route to, send response only if Authorization header with JWT token is present

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var app = express();
 var bodyparser = require('body-parser');
+var expressjwt = require('express-jwt');
 
 var jwt = require('jsonwebtoken');
 var config = require('./config');
@@ -11,11 +12,18 @@ app.set('secret', config.secret);
 
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
+app.use(expressjwt({ secret: config.secret}).unless({path: ['/api/', '/api/request_token']}));
 var router = express.Router();
 
 
 router.get('/', function(req, res) {
     res.send("Hello!");
+})
+
+router.post('/request_token', function(req, res) {
+    var myToken = jwt.sign({username: req.body.username}, config.secret);
+    res.status(200).json(myToken);
+
 })
 
 router.get('/bears', function(req, res) {
